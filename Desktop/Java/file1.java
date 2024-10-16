68bc0d3bfaad40a2ae989be741d46d0bae9a7370
vanshika0101 <vanshikamalik139@gmail.com>
2024-10-16 15:16:32 +0530

    [vanshika] ADD: added files in java folder

---
+++ Desktop/Java/file1.java
@@ -0,0 +1,24 @@
+class Prime{
+public  static void main(Strings[]args){
+int a=5;
+if(isPrime(a)){
+System.out.println(a+"is prime");
+}
+else{
+System.out.println(a+ "is not prime");
+
+}
+}
+public static boolean  isPrime(int a){
+if(a<=1){
+return false;
+}
+for(int i=2;i<a/2;i++){
+if(a%i==0){
+return false;
+}
+}
+return true;
+}
+}
+
